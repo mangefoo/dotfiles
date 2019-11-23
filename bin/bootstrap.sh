f30e5641862b461a97ce783f3e48bf5977aa15f0
Magnus Eriksson <eriksson.mag@gmail.com>
2019-11-23 14:33:04 +0100

    Add oath-toolkit to bootstrap script

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -4,7 +4,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew cask install iterm2 intellij-idea karabiner-elements firefox hammerspoon menumeters little-snitch micro-snitch steam docker vlc spotify nordvpn slack oath-toolkit
-brew install macvim vlc awscli htop
+brew install macvim vlc awscli htop oath-toolkit
 
 # Oh my zsh!
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
