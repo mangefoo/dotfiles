d768e3d6986ccba5a1c67d560dd317222f4a9c71
Magnus Eriksson <eriksson.mag@gmail.com>
2019-11-23 14:29:33 +0100

    Bootstrap script and local zsh config

---
+++ bin/bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew cask install iterm2 intellij-idea karabiner-elements firefox hammerspoon menumeters little-snitch micro-snitch steam docker vlc spotify nordvpn slack oath-toolkit
+brew install macvim vlc awscli htop
+
+# Oh my zsh!
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# SdkMan
+curl -s "https://get.sdkman.io" | bash
+
+# Disable resize animations
+defaults write -g NSWindowResizeTime -float 0.003
+
+# Local zsh configuration
+touch ~/.zshrc.local
+echo "source ~/.zshrc.local" >> ~/.zshrc
